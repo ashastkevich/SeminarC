13772bc75c63614457f41b362c5f135f25d53563
Aleksei <ais_80@inbox.ru>
2022-11-27 13:18:20 +0300

    Task 4 done

---
+++ Task 4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+
+if (a>b && a>c)
+    Console.WriteLine($"max = {a}");
+else if (b>a && b>c)
+    Console.WriteLine($"max = {b}");
+else if (c>a && c>b)
+    Console.WriteLine($"max = {c}");
