2bca0a71d7ac9f89e9ea499dbae93775681d4258
Aleksei <ais_80@inbox.ru>
2022-11-27 17:57:41 +0300

    Task 13 done

---
+++ Task 13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int temp = N;
+int len = 1;
+while (temp/10>0) {
+    len=len*10;
+    temp=temp/10;
+}
+if (len>10)
+    Console.WriteLine($"Третья цифра числа: {N/(len/100)%10}");
+else 
+    Console.WriteLine("Третьей цифры нет");
