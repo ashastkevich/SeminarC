3ed9d167438bb932dfc0663fc14287a617bd4dd6
Aleksei <ais_80@inbox.ru>
2022-11-27 13:09:23 +0300

    Task 2 done

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a>b)
+    Console.WriteLine($"max = {a}");
+else
+    Console.WriteLine($"max = {b}");
