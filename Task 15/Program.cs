f30dd02e36caef33ef60d09284034dd535d1050b
Aleksei <ais_80@inbox.ru>
2022-11-27 18:25:38 +0300

    Task 15 done

---
+++ Task 15/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите день недели: ");
+int N = int.Parse(Console.ReadLine());
+if (N==6 || N==7)
+    Console.WriteLine("Да");
+else 
+    Console.WriteLine("Нет");
