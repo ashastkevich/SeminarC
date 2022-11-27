6e63f6a6dabc9fcac9bcf6fa1b03ef247976fa99
Aleksei <ais_80@inbox.ru>
2022-11-27 19:02:54 +0300

    Task acmp1138 done

---
+++ Task acmp1138/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите числа:");
+int n = int.Parse(Console.ReadLine());
+int max = n;
+n = int.Parse(Console.ReadLine());
+int max2 = n;
+ do {
+    if (n>max) {
+        max2 = max;
+        max = n;
+    }
+    else if (n < max && n > max2)
+        max2 = n;
+    n = int.Parse(Console.ReadLine());
+} while (n != 0);
+Console.WriteLine($"Второй максимум: {max2}");
