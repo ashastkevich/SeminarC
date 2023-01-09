82b1ad0c679e7c9300861d3dcc9cb6545d51a4f7
Aleksei <ais_80@inbox.ru>
2023-01-09 14:11:58 +0300

    Task 25 done

---
+++ Task 25/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Clear();
+Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+int i = 1;
+int result = A;
+while (i < B)
+{
+    result = result * A;
+    i++;
+}
+Console.WriteLine(result);
