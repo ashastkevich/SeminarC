a1181cee300141ba545759a1de875539c319c42f
Aleksei <ais_80@inbox.ru>
2023-01-09 14:30:06 +0300

    Task 29 done

---
+++ Task 29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите элемент массива: ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine($"{string.Join(",      ", array)}");
