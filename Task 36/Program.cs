83dd1ba6e161bdbbace1e8e1c4ea52168a40c4bd
Aleksei <ais_80@inbox.ru>
2023-01-09 22:24:58 +0300

    Task 36 done

---
+++ Task 36/Program.cs
@@ -0,0 +1,25 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1,100);
+}
+
+int CountSum (int[] array)
+{
+    int result = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+            result = result + array[i];
+    }
+    return result;
+}
+
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Массив: {string.Join(",      ", array)}");
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях в массиве: {CountSum(array)}");
