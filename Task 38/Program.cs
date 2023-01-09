6401bb73a0d5f49316a6e50767ae17e800230264
Aleksei <ais_80@inbox.ru>
2023-01-09 22:42:14 +0300

    Task 38 done

---
+++ Task 38/Program.cs
@@ -0,0 +1,27 @@
+﻿void InputArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1,10);
+}
+
+double CountDiff (double[] array)
+{
+    double min = array[0];
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (min > array[i])
+            min = array[i];
+        if (max < array[i])
+            max = array[i];
+    }
+    return max-min;
+}
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine());
+double[] array = new double[n];
+InputArray(array);
+Console.WriteLine($"Массив: {string.Join(",      ", array)}");
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {CountDiff(array)}");
