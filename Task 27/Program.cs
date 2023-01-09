2abc301621197318ae64f68e8937a7946a3e889c
Aleksei <ais_80@inbox.ru>
2023-01-09 14:27:18 +0300

    Task 27 done

---
+++ Task 27/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string? n = Console.ReadLine();
+int result = 0;
+for (int i = 0; i < n.Length; i++)
+{
+    result = result + int.Parse(n[i].ToString());
+}
+Console.WriteLine(result);
