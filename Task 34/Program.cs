784b51650ff6017a4611c01104ab3fb3ad6a086d
Aleksei <ais_80@inbox.ru>
2023-01-09 22:19:13 +0300

    Task 34 done

---
+++ Task 34/Program.cs
@@ -0,0 +1,25 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100,1000);
+}
+
+int CountEven (int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            result++;
+    }
+    return result;
+}
+
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Массив: {string.Join(",      ", array)}");
+Console.WriteLine($"Количество четных чисел в массиве: {CountEven(array)}");
