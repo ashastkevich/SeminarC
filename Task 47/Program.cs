b18f7c375f0e5627262c5cfea39d22d33d48b1a7
Aleksei <ais_80@inbox.ru>
2023-01-15 20:45:15 +0300

    Task 47 done

---
+++ Task 47/Program.cs
@@ -0,0 +1,19 @@
+﻿void InputArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().NextDouble()*(20-1)+1;
+            Console.Write($"{array[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+string[] size = Console.ReadLine().Split(" ");
+int row = int.Parse(size[0]);
+int column = int.Parse(size[1]);
+double[,] array = new double[row,column];
+InputArray(array);
