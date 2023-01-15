efa84a12f9b997b3f47082db070e8e884156a4eb
Aleksei <ais_80@inbox.ru>
2023-01-15 17:54:20 +0300

    Task 41 done

---
+++ Task 41/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите количество чисел: ");
+int M = int.Parse(Console.ReadLine());
+int Number = 0;
+int count = 0;
+for (int i = 0; i < M; i++)
+{
+    Console.Write("Введите число: ");
+    if (int.Parse(Console.ReadLine()) > 0)
+        count++;
+}
+Console.WriteLine($"Количество чисел больше нуля: {count}");
