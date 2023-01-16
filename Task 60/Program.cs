3c73b55be313dc4294c8dceb360485e76458b40d
Aleksei <ais_80@inbox.ru>
2023-01-16 20:40:58 +0300

    Task 60 done

---
+++ Task 60/Program.cs
@@ -0,0 +1,35 @@
+﻿void InputMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            for (int j = 0; j < matrix.GetLength(2); j++)
+            {
+                matrix[i, k, j] = new Random().Next(0, 100);
+                Console.Write($"{matrix[i, k, j]} \t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Clear();
+
+int[,,] matrix = new int[2, 2, 2];
+
+InputMatrix(matrix);
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int k = 0; k < matrix.GetLength(1); k++)
+    {
+        for (int j = 0; j < matrix.GetLength(2); j++)
+        {
+            Console.Write($"{matrix[i, k, j]} ({i},{k},{j}) \t");
+        }
+        Console.WriteLine();
+    }
+}
