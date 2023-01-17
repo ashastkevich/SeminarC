8c0cc54edbc4e4262cce1f0b26a893687d3bfec7
Aleksei <ais_80@inbox.ru>
2023-01-17 17:17:05 +0300

    Task 66 done

---
+++ Task 66/Program.cs
@@ -0,0 +1,12 @@
+﻿int SumMN(int a, int b)
+{
+    int res = b;
+    if (a != b) res = res + SumMN(a, b - 1);
+    return res;
+}
+Console.Clear();
+Console.Write("Введите M: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма числе в промежутке между M и N равна: {SumMN(M, N)}");
